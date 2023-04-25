19a16cd8cdf5dc91c51e2f1756f9f17d5bccd086
AudunSorheim <audun.sorheim@nav.no>
2023-04-25 15:48:39 +0200

    wip

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -3,13 +3,7 @@ import App from "./App";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  margin: auto;
   max-width: 444px;
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: 1fr;
-  align-items: center;
-  justify-content: center;
 `;
 
 const Mikrofrontend = () => {
